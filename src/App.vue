e5cdc9dc3132129d2818bd25c5a93f4ae7cdee69
Onur <ob-vest@hotmail.com>
2022-12-19 19:52:31 +0100

    added descriptionblock

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import DescriptionBlock from "./components/DescriptionBlock.vue";
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <img class="" src="./assets/background.webp" alt="" />
-  </div>
-  <section class="flex flex-row m-20 justify-center">
-    <aside class="mr-5">
-      <img
-        class="w-80 h-52 object-cover rounded-lg"
-        src="./assets/background.webp"
-        alt=""
-      />
-    </aside>
-    <div class="w-1/2">
-      <h1 class="text-center underline text-3xl mb-3">What we do</h1>
-      <p>
-        GreenLead is a company that specializes in making green visions a
-        reality, partly in cooperation with investors and also in cooperation
-        with the industry in the transition to green energy. GreenLead advises
-        on renewable energy systems - We have a large portfolio of projects and
-        a broad network of investors - we often work in the empty space between
-        financing and the project. We specialize in designing green projects and
-        describing the project to investors as well as finding the right
-        projects for the right investor. This means making the green transition
-        a reality.
-      </p>
-    </div>
-  </section>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+
+  <DescriptionBlock image-url="background.webp" />
 </template>
---
+++ src/components/DescriptionBlock.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  imageUrl: String,
+});
+</script>
+
+<template>
+  <section
+    class="flex flex-col md:flex-row m-5 md:m-20 justify-center items-center md:items-start"
+  >
+    <aside class="mb-5 md:mr-5">
+      <img class="w-80 h-52 object-cover rounded-lg" :src="imageUrl" alt="" />
+    </aside>
+    <div class="md:w-1/2">
+      <h1 class="text-center underline text-3xl mb-3">What we do</h1>
+      <p>
+        GreenLead is a company that specializes in making green visions a
+        reality, partly in cooperation with investors and also in cooperation
+        with the industry in the transition to green energy. GreenLead advises
+        on renewable energy systems - We have a large portfolio of projects and
+        a broad network of investors - we often work in the empty space between
+        financing and the project. We specialize in designing green projects and
+        describing the project to investors as well as finding the right
+        projects for the right investor. This means making the green transition
+        a reality.
+      </p>
+    </div>
+  </section>
+</template>
